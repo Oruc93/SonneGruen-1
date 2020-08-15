3ffe58a2fd463e4513d7efe121800aa680e832dc
Benjamin Schlotter <benji@schlotterfam.de>
2020-08-15 17:34:45 +0200

    simple fix for 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,12 +4,12 @@ import Layout from "../components/layout";
 
 function NotFoundPage() {
   return (
-    <>
+    <Layout headerClass="bg-white relative">
       <div className="min-h-screen flex flex-col items-center align-center justify-center text-fold">
         <h1 className="text-5xl">Not Found</h1>
         <p>You just hit a page that doesn't exist...</p>
       </div>
-    </>
+    </Layout>
   );
 }
 
